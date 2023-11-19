a8134370abcafb0813817505661b91a1bf9e5e08
kacper-lyczba <kacper.lyczba@gmail.com>
2023-11-19 08:50:11 +0100

    feat: outdated tests removal + github pages deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { configDefaults } from 'vitest/config';
 
 /* eslint-disable import/no-default-export */
 export default defineConfig({
+  base: '/react_tsh_store/',
   plugins: [
     react({
       jsxImportSource: '@welldone-software/why-did-you-render',
